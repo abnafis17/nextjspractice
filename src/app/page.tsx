02c05c745f47ada7f58b1c37c38b143c13457358
abnafis17 <bakhtiyarnafis17@gmail.com>
2025-10-31 21:54:14 +0600

    Add Zustand state management to Home component and update package dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+"use client";
+
+import React from "react";
+import { create } from "zustand";
+
+type Store = {
+  count: number;
+  increment: () => void;
+};
+
+const useStore = create<Store>((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+}));
 
 export default function Home() {
+  const { count, increment } = useStore();
+
   return (
     <>
       <div> Home Page</div>
+      <div>Count: {count}</div>
+      <button
+        className="border-2 px-4 py-2 hover:cursor-pointer"
+        onClick={increment}
+      >
+        Increment
+      </button>
     </>
   );
 }
